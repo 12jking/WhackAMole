f623d55762f9dc4c68c423e71d1906dc442cb536
12jking <12jkinghb@gmail.com>
2021-03-25 12:33:22 +0100

    Added minus points

--- src/main/java/de/j/whackamole/listener/PlayerInteractListener.java
+++ src/main/java/de/j/whackamole/listener/PlayerInteractListener.java
@@ -1,5 +1,7 @@
 package de.j.whackamole.listener;
 
+import de.j.whackamole.commands.StartCommand;
+import de.j.whackamole.util.Game;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,8 +14,15 @@ public class PlayerInteractListener implements Listener {
 
     @EventHandler
     public void onInteract(PlayerInteractEvent event) {
-        if (!(event.getAction() == Action.RIGHT_CLICK_BLOCK)) return;
-        if (!(Objects.requireNonNull(event.getClickedBlock()).getType() == Material.BLACK_SHULKER_BOX)) return;
-        event.setCancelled(true);
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if (!(Objects.requireNonNull(event.getClickedBlock()).getType() == Material.BLACK_SHULKER_BOX)) return;
+            event.setCancelled(true);
+        } else if (event.getAction() == Action.LEFT_CLICK_BLOCK) {
+            if (StartCommand.running) {
+                Game.addScore(event.getPlayer(), - 1);
+            }
+            event.setCancelled(true);
+        }
+
     }
 }
